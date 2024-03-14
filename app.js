4a4576e4b5af1bba89286030f98073eef950c1f0
Makxmud <161556102+makhmudev@users.noreply.github.com>
2024-03-14 11:42:50 +0500

    nice done

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const burgerBtn = document.querySelector(".burger");
-
-burgerBtn.addEventListener("click", ()=>{
-    
-})
+const burgerBox = document.querySelector(".burger-box");
+const menuItems = document.querySelectorAll(".menu");
+burgerBtn.addEventListener("click", () => {
+  burgerBox.classList.toggle("active");
+  if (burgerBox.classList.contains("active")) {
+    menuItems.forEach((menuItem, index) => {
+      menuItem.classList.add(`active${index + 1}`);
+      console.log(`active${index + 1}`);
+    });
+  } else {
+    menuItems.forEach((menuItem, index) => {
+      menuItem.classList.remove(`active${index + 1}`);
+      console.log(`active${index + 1}`);
+    });
+  }
+});
